0d53eb1bfa3ccbf497c9325171951c85a88d685a
Johanna <2myadverts@gmail.com>
2020-08-26 21:32:46 +0200

    starting signup and login

--- config/routes.js
+++ config/routes.js
@@ -1,14 +1,27 @@
 const express = require('express');
 const router = express.Router();
 const multer = require('multer');
-const userController = require('../controllers/user.controller');
+const usersController = require('../controllers/user.controller');
 const commentController = require('../controllers/user.controller');
 const sessionMiddleware = require('../middlewares/session.middleware');
 const upload = require('../config/multer.config');
 
 const passport = require('passport');
 
-router.get('/', userController.showLogin);
+router.get('/signup', sessionMiddleware.isNotAuthenticated, usersController.showSignupPage); 
+router.post('/signup', sessionMiddleware.isNotAuthenticated, upload.single('avatar'), usersController.createUser);
+router.get('/activate/:token', sessionMiddleware.isNotAuthenticated, usersController.activateUser);
+router.get('/login', sessionMiddleware.isNotAuthenticated, usersController.showLoginPage);
+router.post('/login', sessionMiddleware.isNotAuthenticated, usersController.doLogin);
+router.get('/auth/slack', sessionMiddleware.isNotAuthenticated, usersController.loginWithSlack);
+router.get('/auth/google', sessionMiddleware.isNotAuthenticated, usersController.loginWithGmail);
+router.get('/auth/google/callback', sessionMiddleware.isNotAuthenticated, usersController.getLoginWithGmail);
+router.get('/logout', sessionMiddleware.isAuthenticated, usersController.logout);
+
+
+router.get('/', (req, res, next) => {
+    res.render('index');
+});
 
 module.exports = router;
 
--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,3 +1,203 @@
-module.exports.showLogin = (req, res, next) => {
-    res.render('index');
+const mongoose = require('mongoose');
+const User = require('../models/owner.model');
+const nodemailer = require('../config/mailer.config');
+const passport = require('passport');
+
+module.exports.showSignupPage = (req, res, next) => {
+    res.render('users/userform')
 }
+
+module.exports.showLoginPage = (req, res, next) => {
+    res.render('users/login')
+}
+
+module.exports.doLogin = (req, res, next) => {
+    User.findOne({ email: req.body.email })
+        .then(user => {
+            if (user) {
+                user.checkPassword(req.body.password)
+                    .then(match => {
+                        if (match) {
+                            if (user.activation.active) {
+                                req.session.userId = user._id;
+                                res.redirect('/index')
+                        } else {
+                            res.render('users/login', {
+                                error: {
+                                    validation: {
+                                        message: 'Your account is not active, check your email!'
+                                    }
+                                }
+                            })
+                        }
+                        } else {
+                        res.render('users/login', {
+                            error: {
+                                email: {
+                                    message: 'user not found'
+                                }
+                            }
+                        })
+                        }
+                    })
+            } else {
+                res.render("users/login", {
+                    error: {
+                        email: {
+                            message: "user not found",
+                        },
+                    },
+                });
+            }
+        })
+        .catch(next)
+}
+
+module.exports.loginWithSlack = (req, res, next) => {
+    const passportSlackController = passport.authenticate('slack', (error, user) => {
+      if (error) {
+        next(error);
+      } else {
+        req.session.userId = user._id;
+        res.redirect('/projects');
+      }
+    })
+    
+    passportSlackController(req, res, next);
+}
+  
+module.exports.loginWithGmail = (req, res, next) => {
+    const passportGoogleLogin = passport.authenticate('google', {
+        scope: [
+        "https://www.googleapis.com/auth/userinfo.profile",
+        "https://www.googleapis.com/auth/userinfo.email"
+        ]
+    })
+    passportGoogleLogin(req, res, next)
+}
+
+module.exports.getLoginWithGmail = (req, res, next) => {
+    const passportGoogleController =  passport.authenticate('google', { scope:  ['profile', 'email']}, (error, user) => {
+        if (error) {
+            next(error);
+        } else {
+            req.session.userId = user._id;
+            res.redirect('/projects');
+        }
+    })
+    passportGoogleController(req, res, next)
+}
+
+
+module.exports.createUser = (req, res, next) => {
+    const userParams = req.body;
+    userParams.avatar = req.file ? req.file.path : undefined;
+    const user = new User(userParams);
+
+    user.save()
+        .then(user => {
+            nodemailer.sendValidationEmail(user.email, user.activation.token, user.name);
+            res.render('users/login', {
+                message: 'Check your email for activation'
+            })
+        })
+        .catch((error) => {
+            if (error instanceof mongoose.Error.ValidationError) {
+                res.render("users/userform", { error: error.errors, user });
+            } else if (error.code === 11000) { // error when duplicated user
+                res.render("users/userform", {
+                user,
+                error: {
+                    email: {
+                        message: 'user already exists'
+                    }
+                }
+                });
+            } else {
+                next(error);
+            }
+            })
+        .catch(next)
+}
+
+module.exports.activateUser = (req, res, next) => {
+    User.findOne({ "activation.token": req.params.token })
+        .then(user => {
+            if (user) {
+                user.activation.active = true;
+                user.save()
+                    .then(() => {
+                        res.render('users/login', {
+                            message: 'Your account has been activated. You can log in now.'
+                        })
+                    })
+                    .catch(err => next(err))
+            } else {
+                res.render('users/login', {
+                    error: {
+                        validation: {
+                            message: 'Invalid link'
+                        }
+                    }
+                })
+            }
+        })
+        .catch(err => next(err))
+}
+
+module.exports.logout = (req, res, next) => {
+    req.session.destroy()
+
+    res.redirect('/login')
+}
+
+module.exports.showUserProfilePage = (req, res, next) => {
+    const { id } = req.params;
+    User.findById(id)
+        .populate('projects')
+        .then(user => {
+            res.render('users/user', { user })
+        })
+        .catch(err => next(err))
+}
+
+module.exports.showEditProfile = (req, res, next) => {
+    const { id } = req.params;
+    User.findById(id)
+        .then(user => {
+            res.render('users/userform', { user })
+        })
+        .catch(err => next(err))
+}
+
+module.exports.updateProfile = (req, res, next) => {
+    const userParams = req.body;
+
+    if( req.file ) {
+        userParams.avatar = req.file.path;
+    }
+
+    User.findByIdAndUpdate(req.params.id, userParams, { runValidators: true, new: true })
+        .then(user => {
+            if (user) {
+                res.redirect(`/user/${user._id}`)
+            } else {
+                res.redirect('/projects')
+            }
+        })
+        .catch(err => next(err))
+}
+
+module.exports.deleteProfile = (req, res, next) => {
+    User.findByIdAndDelete(req.params.id)
+        .then(() => {
+            req.currentUser.remove()
+              .then(() => {
+                req.session.destroy()
+                res.redirect('/login')
+              })
+              .catch(err => next(err))
+        })
+        .catch(err => next(err))
+}
+
