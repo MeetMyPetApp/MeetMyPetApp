c3f9ac48958740a0edc77200cee8a9df07685824
Johanna <2myadverts@gmail.com>
2020-09-14 15:58:25 +0200

    fixing like

--- public/js/index.js
+++ public/js/index.js
@@ -1,3 +1,13 @@
+document.querySelectorAll("[data-like-post]").forEach(el => {
+    el.addEventListener("click", function () {
+        axios.get(`/post/${this.dataset.likePost}/like`)
+            .then(response => {
+                const likesContainer = this.querySelector(".likes-count")
+                likesContainer.innerText = Number(likesContainer.innerText) + response.data.like
+            })
+    })
+  })
+
 document.querySelectorAll("[data-match-status]").forEach(el => {
     el.addEventListener("click", function (event) {
         const matchBtn = event.currentTarget;
